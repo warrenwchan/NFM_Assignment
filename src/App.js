5cd80369ac599d1d4beb4ed3daff29d7f84c8023
Warren-Ipro <warren@iprogroup.ca>
2022-11-16 00:35:56 -0800

    Uploading project files to github.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './styles/App.css';
 
-function App() {
+function App(props) {
+  const { data } = props;
+  const [projectsData, setProjectData] = useState(data.projects);
+  const [selectedProjects, setSelectedProjects] = useState([]);
+
+  const handleSortDate = () => {
+    // Sorts date from earliest by earliest.
+    var sortedDate = [...projectsData.sort((a, b) => new Date(a.postedDate.replaceAll(',', '')) - new Date(b.postedDate.replaceAll(',', '')))]
+    setProjectData(projectsData => sortedDate)
+  }
+
+  const handleSelect = (e) => {
+    // Checks to see if input checkbox is checked before adding or removing projects from a selected array in state.
+    var check = e.target.checked;
+    check ? setSelectedProjects(selectedProjects => [...selectedProjects , projectsData[e.target.value]]) : setSelectedProjects((selectedProjects) => selectedProjects.filter((project) => project.name !== projectsData[e.target.value].name))
+  }
+
+  const handleSubmit = () => {
+    // Checks to see if any projects are submitted and console logs selected projects. Alert error message if nothing is selected.
+    selectedProjects > 0 ? console.log(selectedProjects) : alert("No projects selected");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className='caption'>
+          <h1>Projects</h1>
+        </div>
+        <table>
+          <thead className='table-header'>
+            <tr className='table-row secondary-text'>
+              <th className="table-header-cell">Name</th>
+              <th className="table-header-cell">type</th>
+              <th className="table-header-cell">Casting Director</th>
+              <th className="table-header-cell"><button className='sort-button secondary-text' type='button' onClick={handleSortDate} >Posted Date</button></th>
+            </tr>
+          </thead>
+          <tbody>
+            {projectsData.map((project, i) => {
+              return (
+                <tr className='table-row primary-text' key={i}>
+                  <td className='table-row-check'><input type="checkbox" name='select' value={i} onChange={handleSelect} data-value={project} /></td>
+                  <td className='table-row-cell uppercase'><div>{project.name}</div></td>
+                  <td className='table-row-cell'>{project.type}</td>
+                  <td className='table-row-cell'>{project.castingDirector}</td>
+                  <td className='table-row-cell'>{project.postedDate}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+        <div className="btn-container">
+          <button onClick={handleSubmit} className='submit-button'>Submit</button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//Data
+import projects from './data/projects.json';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      data={projects}
+    />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
